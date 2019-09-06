237cff974e3ffec779e80740cab95ff7ab3a2111
15511603197 <15511603197>
2019-09-06 10:10:21 +0800

    init

---
+++ src/AppModel/Admin.php
@@ -0,0 +1,186 @@
+<?php declare(strict_types=1);
+
+
+namespace App\Models\Entity;
+
+use Swoft\Db\Annotation\Mapping\Column;
+use Swoft\Db\Annotation\Mapping\Entity;
+use Swoft\Db\Annotation\Mapping\Id;
+use Swoft\Db\Eloquent\Model;
+
+
+/**
+ *
+ * Class Admin
+ * php bin/swoft entity:create order_goods --path=@app/Models/Entity --field_prefix=hsh_
+ * php bin/swoft entity:create goods_img --path=@app/Models/Entity --field_prefix=hsh_
+ * @since 2.0
+ *
+ * @Entity(table="admin")
+ */
+class Admin extends Model
+{
+    /**
+     *
+     * @Id()
+     * @Column()
+     *
+     * @var int
+     */
+    private $id;
+
+    /**
+     * 账号
+     *
+     * @Column()
+     *
+     * @var string|null
+     */
+    private $name;
+
+    /**
+     * 密码
+     *
+     * @Column(hidden=true)
+     *
+     * @var string|null
+     */
+    private $password;
+
+    /**
+     * 创建时间
+     *
+     * @Column(name="created_at", prop="createdAt")
+     *
+     * @var string|null
+     */
+    private $createdAt;
+
+    /**
+     * 更新时间
+     *
+     * @Column(name="updated_at", prop="updatedAt")
+     *
+     * @var string|null
+     */
+    private $updatedAt;
+
+    /**
+     * 删除时间
+     *
+     * @Column(name="deleted_at", prop="deletedAt")
+     *
+     * @var string|null
+     */
+    private $deletedAt;
+
+
+    /**
+     * @param int $id
+     *
+     * @return void
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @param string|null $name
+     *
+     * @return void
+     */
+    public function setName(?string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @param string|null $password
+     *
+     * @return void
+     */
+    public function setPassword(?string $password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @param string|null $createdAt
+     *
+     * @return void
+     */
+    public function setCreatedAt(?string $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @param string|null $updatedAt
+     *
+     * @return void
+     */
+    public function setUpdatedAt(?string $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    /**
+     * @param string|null $deletedAt
+     *
+     * @return void
+     */
+    public function setDeletedAt(?string $deletedAt): void
+    {
+        $this->deletedAt = $deletedAt;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getCreatedAt(): ?string
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getUpdatedAt(): ?string
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getDeletedAt(): ?string
+    {
+        return $this->deletedAt;
+    }
+
+}
